49af2d466b11d75265938c106a6ab28d2b538a31
naveenkchelluboina <naveenkchelluboina@gmail.com>
2019-12-26 14:36:13 +0530

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { HomeComponent } from './home/home.component';
 
+const routes: Route = [
+  {path: 'home', component:HomeComponent},
+  { path: 'course', component: CourseComponent },
+]
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(routes) ],
   declarations: [ AppComponent, CourseComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
